c5166b2e022cec6f7f60306ab8aa99ab735ddf97
Skripko Andrey <itu.ccb@gmail.com>
2020-07-19 23:48:05 +0300

    remove debug

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import { payload as data } from '../data/payload';
+// import { payload as data } from '../data/payload';
 // import { payload2 as data } from '../data/payload.2';
 
 import { Game } from './domain/Game/Game';
 
 
 const game = new Game(7070);
-if (window) (window as any).game = game;
+// if (window) (window as any).game = game;
 
-console.log(JSON.stringify(data.map(game.handleData), null, 2));
+// console.log(JSON.stringify(data.map(game.handleData), null, 2));
